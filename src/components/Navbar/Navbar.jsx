865bdd51fef055139f94875b2cfa50ce0877d2ab
tanishqkrk <tanishqkrk@gmail.com>
2022-12-06 23:47:36 +0530

    added ham design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavbarLink from './NavbarLink/NavbarLink'
 import './Navbar.css'
 const Navbar = () => {
+
+    const [hamburger, setHamburger] = useState(true);
+
+
     return (
         <div className="navbar">
             <NavbarLink title={'//  Experience'} href={'#experience'}></NavbarLink>
@@ -9,13 +13,17 @@ const Navbar = () => {
             <a href="#"> <img src="./opt 1.svg" alt="" className="navbar__logo" /></a>
             <NavbarLink title={'//  Technologies'} href={'#technologies'}></NavbarLink>
             <NavbarLink title={'//  Blogs'} href={'#blogs'}></NavbarLink>
+            <div onClick={ } className="hamburger">
+                <div className="hamburger__line1"></div>
+                <div className="hamburger__line2"></div>
+            </div>
             <div className="navbar__ham">
                 <NavbarLink title={'Projects'} href={'#projects'}></NavbarLink>
                 <NavbarLink title={'Experience'} href={'#projects'}></NavbarLink>
                 <NavbarLink title={'Blogs'} href={'#projects'}></NavbarLink>
                 <NavbarLink title={'Contact'} href={'#projects'}></NavbarLink>
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/Projects/GithubProject/GithubProject.jsx
+++ src/components/Projects/GithubProject/GithubProject.jsx
@@ -11,7 +11,7 @@ const GithubProject = ({ name, desc, github }) => {
         <div className='githubproject'>
             <div data-aos="fade-down" data-aos-duration="1000" className="githubproject__title">{name}</div>
             <div data-aos="fade-down" data-aos-duration="1000" className="githubproject__desc">{desc}</div>
-            <div data-aos="fade-down" data-aos-duration="1000" className="githubproject__github"><a target={'_blank'} href={github} className="githubproject__github--link"><i class="fa-brands fa-github"></i></a></div>
+            <div data-aos="fade-down" data-aos-duration="1000" className="githubproject__github"><a target={'_blank'} href={github} className="githubproject__github--link"><i className="fa-brands fa-github"></i></a></div>
         </div>
     )
 }
