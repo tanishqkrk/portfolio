4c999e2e4cf1ed4ffd63a7392f48481c08e13fac
tanishqkrk <tanishqkrk@gmail.com>
2022-12-07 00:07:21 +0530

    bug

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,7 @@ import React, { useState } from 'react'
 import NavbarLink from './NavbarLink/NavbarLink'
 import './Navbar.css'
 const Navbar = () => {
-
-    const [hamburger, setHamburger] = useState(true);
-
-
+    // const [hamburger, setHamburger] = useState(true);
     return (
         <div className="navbar">
             <NavbarLink title={'//  Experience'} href={'#experience'}></NavbarLink>
@@ -13,7 +10,7 @@ const Navbar = () => {
             <a href="#"> <img src="./opt 1.svg" alt="" className="navbar__logo" /></a>
             <NavbarLink title={'//  Technologies'} href={'#technologies'}></NavbarLink>
             <NavbarLink title={'//  Blogs'} href={'#blogs'}></NavbarLink>
-            <div onClick={ } className="hamburger">
+            <div className="hamburger">
                 <div className="hamburger__line1"></div>
                 <div className="hamburger__line2"></div>
             </div>
