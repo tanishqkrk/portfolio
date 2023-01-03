f11c7ed8b45852539d39b4bed77f6fff0bebb61c
tanishqkrk <tanishqkrk@gmail.com>
2023-01-03 17:05:08 +0530

    added notification

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,32 @@ import Resume from './components/Resume/Resume'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
+  const notification = useRef();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      notification.current.style.animation = 'disappear 8s'
+    }, 3000)
+    return () => clearTimeout(timer)
+  })
+
   const overlay = useRef();
   const hoverOverlay = (e) => {
     let rect = overlay.current.getBoundingClientRect();
     let mouseX = e.clientX - rect.left;
     let mouseY = e.clientY - rect.top;
     overlay.current.style.opacity = `1`;
-    overlay.current.style.background = `radial-gradient(circle at  ${mouseX}px ${mouseY}px, rgba(255,255,255,0.04) 0%, transparent calc(6% + 0px)) no-repeat border-box border-box`;
+    overlay.current.style.background = `radial-gradient(circle at  ${mouseX}px ${mouseY}px, rgba(255,255,255,0.05) 0%, transparent calc(5% + 0px)) no-repeat border-box border-box`;
     // console.log(1)
   }
 
 
   return (
     <div ref={overlay} onMouseMove={hoverOverlay} className='app'>
+      <div ref={notification} className="notification">
+        <h4>Disclaimer</h4>
+        <p>This website uses features that are relatively new and advanced for some browser, please update your browser to latest version to experience completely.</p>
+        {/* <button>✅</button> */}
+      </div>
       <Navbar />
       <Socials />
       <Resume />
