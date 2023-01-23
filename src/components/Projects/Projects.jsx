e5588b022af2bba8210e7cec439ed473d575fd5e
tanishqkrk <tanishqkrk@gmail.com>
2023-01-24 05:14:53 +0530

    NEW PROJECT LESSGOOOOOOOOOOO

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import './Project/Project.css'
 import './Projects.css'
 import GithubProject from './GithubProject/GithubProject';
 import './GithubProject/GithubProject.css'
+import projects from '../../projects.json'
 
 const Projects = () => {
     // useEffect(() => {
@@ -18,9 +19,14 @@ const Projects = () => {
                 <div className="projects__desc">I've developed a lot of personal side projects some, my favourites are as below:</div>
             </div>
             <div className="projects__work">
-                <Project name={'IP Address Tracker'} desc={"Track any IP address, get its location, vpn status and ISP"} github={'https://github.com/tanishqkrk/IP-tracker'} img={'./ip-min.png'} href={'https://tanishqkrk.github.io/IP-tracker '} ></Project>
-                <Project position={'right'} name={'Password Generator'} desc={"Generate passwords and provide your own parameters (Uppercase, Lowercase, Number and Symbols)"} github={'https://github.com/tanishqkrk/password-generator'} img={'./pg-min.png'} href={'https://tanishqkrk.github.io/password-generator '} ></Project>
-                <Project name={'Card checkout front-end'} desc={"Front-end for a card details checkout page"} github={'https://github.com/tanishqkrk/password-generator'} img={'./card-min.png'} href={'https://tanishqkrk.github.io/password-generator '} ></Project>
+                {
+                    projects.map(project => <Project name={project.name} desc={project.desc} github={`https://github.com/tanishqkrk/${project.github}`} img={`./${project.img}`} href={project.href} ></Project>)
+                }
+
+
+                {/* <Project name={'IP Address Tracker'} desc={"Track any IP address, get its location, vpn status and ISP"} github={'https://github.com/tanishqkrk/IP-tracker'} img={'./ip-min.png'} href={'https://tanishqkrk.github.io/IP-tracker '} ></Project> */}
+                {/* <Project position={'right'} name={'Password Generator'} desc={"Generate passwords and provide your own parameters (Uppercase, Lowercase, Number and Symbols)"} github={'https://github.com/tanishqkrk/password-generator'} img={'./pg-min.png'} href={'https://tanishqkrk.github.io/password-generator '} ></Project> */}
+                {/* <Project name={'Card checkout front-end'} desc={"Front-end for a card details checkout page"} github={'https://github.com/tanishqkrk/password-generator'} img={'./card-min.png'} href={'https://tanishqkrk.github.io/password-generator '} ></Project> */}
             </div>
             <div className="githubProjects__title">Other projects you may find intersting!</div>
             <div className="github"   >
